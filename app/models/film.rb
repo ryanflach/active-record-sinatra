f0d2bb5647e3ca0ea9a7f9fb6c81dd0ee30c43b4
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-05 16:52:01 -0600

    Add class methods to calculate director's total sales and average box office

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,4 +2,12 @@ class Film < ActiveRecord::Base
   #Says that a Film belongs to only one Genre and allows you to call .genre on a Film object, same for Director
   belongs_to :genre
   belongs_to :director
+
+  def self.total_sales
+    sum(:box_office_sales)
+  end
+
+  def self.average_gross
+    average(:box_office_sales)
+  end
 end
