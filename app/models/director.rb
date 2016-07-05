9d9404241b95865ce39aff3a13d7e6640d16bf41
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-05 11:29:56 -0600

    Add Director table and model

---
+++ app/models/director.rb
@@ -0,0 +1,3 @@
+class Director < ActiveRecord::Base
+  has_many :film
+end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,4 @@
 class Film < ActiveRecord::Base
-  #Says that a Film belongs to only one Genre and allows you to call .genre on a Film object
-  belongs_to :genre
+  #Says that a Film belongs to only one Genre and allows you to call .genre on a Film object, same for Director
+  belongs_to :genre, :director
 end
